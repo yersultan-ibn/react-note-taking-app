6b47ccb7a52aa7ce4d972f2a14a36c99a6de07ae
Yersultan <ersultansansyzbaj@gmail.com>
2023-05-28 16:14:34 +0600

    finish

--- src/NewNote.tsx
+++ src/NewNote.tsx
@@ -1,11 +1,21 @@
-import React from "react";
-import { NoteForm } from "./NoteForm";
+import { NoteData, Tag } from "./App"
+import { NoteForm } from "./NoteForm"
 
-export const NewNote = () => {
+type NewNoteProps = {
+  onSubmit: (data: NoteData) => void
+  onAddTag: (tag: Tag) => void
+  availableTags: Tag[]
+}
+
+export function NewNote({ onSubmit, onAddTag, availableTags }: NewNoteProps) {
   return (
     <>
       <h1 className="mb-4">New Note</h1>
-      <NoteForm/>
+      <NoteForm
+        onSubmit={onSubmit}
+        onAddTag={onAddTag}
+        availableTags={availableTags}
+      />
     </>
-  );
-};
+  )
+}
--- src/NoteForm.tsx
+++ src/NoteForm.tsx
@@ -1,26 +1,39 @@
-import React, { FormEvent, useRef, useState } from "react";
-import { Col, Stack, Row, Form, Button } from "react-bootstrap";
-import { Link } from "react-router-dom";
-import CreatableReactSelect from "react-select/creatable";
-import { NoteData, Tag } from "./App";
+import { FormEvent, useRef, useState } from "react"
+import { Button, Col, Form, Row, Stack } from "react-bootstrap"
+import { Link, useNavigate } from "react-router-dom"
+import CreatableReactSelect from "react-select/creatable"
+import { NoteData, Tag } from "./App"
+import { v4 as uuidV4 } from "uuid"
 
 type NoteFormProps = {
-  onSubmit: (data: NoteData) => void;
-};
+  onSubmit: (data: NoteData) => void
+  onAddTag: (tag: Tag) => void
+  availableTags: Tag[]
+} & Partial<NoteData>
 
-export const NoteForm = ({ onSubmit }: NoteFormProps) => {
-  const titleRef = useRef<HTMLInputElement>(null);
-  const markdownRef = useRef<HTMLTextAreaElement>(null);
-  const [selectTags, setSelectTags] = useState<Tag[]>();
+export function NoteForm({
+  onSubmit,
+  onAddTag,
+  availableTags,
+  title = "",
+  markdown = "",
+  tags = [],
+}: NoteFormProps) {
+  const titleRef = useRef<HTMLInputElement>(null)
+  const markdownRef = useRef<HTMLTextAreaElement>(null)
+  const [selectedTags, setSelectedTags] = useState<Tag[]>(tags)
+  const navigate = useNavigate()
 
   function handleSubmit(e: FormEvent) {
-    e.preventDefault();
+    e.preventDefault()
 
     onSubmit({
       title: titleRef.current!.value,
       markdown: markdownRef.current!.value,
-      tags: [],
-    });
+      tags: selectedTags,
+    })
+
+    navigate("..")
   }
 
   return (
@@ -30,22 +43,30 @@ export const NoteForm = ({ onSubmit }: NoteFormProps) => {
           <Col>
             <Form.Group controlId="title">
               <Form.Label>Title</Form.Label>
-              <Form.Control ref={titleRef} required />
+              <Form.Control ref={titleRef} required defaultValue={title} />
             </Form.Group>
           </Col>
           <Col>
             <Form.Group controlId="tags">
-              <Form.Label>Title</Form.Label>
+              <Form.Label>Tags</Form.Label>
               <CreatableReactSelect
-                value={selectTags.map((tag) => {
-                  return { label: tag.label, value: tag.id };
+                onCreateOption={label => {
+                  const newTag = { id: uuidV4(), label }
+                  onAddTag(newTag)
+                  setSelectedTags(prev => [...prev, newTag])
+                }}
+                value={selectedTags.map(tag => {
+                  return { label: tag.label, value: tag.id }
                 })}
-                onChange={(tags) => {
-                  setSelectTags(
-                    tags.map((tag) => {
-                      return { label: tag.label, id: tag.value };
+                options={availableTags.map(tag => {
+                  return { label: tag.label, value: tag.id }
+                })}
+                onChange={tags => {
+                  setSelectedTags(
+                    tags.map(tag => {
+                      return { label: tag.label, id: tag.value }
                     })
-                  );
+                  )
                 }}
                 isMulti
               />
@@ -53,8 +74,14 @@ export const NoteForm = ({ onSubmit }: NoteFormProps) => {
           </Col>
         </Row>
         <Form.Group controlId="markdown">
-          <Form.Label>Title</Form.Label>
-          <Form.Control required as="textarea" ref={markdownRef} rows={15} />
+          <Form.Label>Body</Form.Label>
+          <Form.Control
+            defaultValue={markdown}
+            required
+            as="textarea"
+            ref={markdownRef}
+            rows={15}
+          />
         </Form.Group>
         <Stack direction="horizontal" gap={2} className="justify-content-end">
           <Button type="submit" variant="primary">
@@ -68,5 +95,5 @@ export const NoteForm = ({ onSubmit }: NoteFormProps) => {
         </Stack>
       </Stack>
     </Form>
-  );
-};
+  )
+}
